d694d3652d61b2b80d3cf058a7c19a706b7df1ea
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-12-23 14:27:14 -0300

    Commit 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace csharp_orientacao_objetos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // 
+    }
+  }
+}
