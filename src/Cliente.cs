483341c8b9969ecb551fc8c4f426168a67c060a5
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-12-26 15:33:46 -0300

    Commit 5

---
+++ src/Cliente.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+  public class Cliente
+  {
+    private string nome = "";
+    private string cpf = "";
+    private string profissao = "";
+
+    public void setNome(string nome)
+    {
+      this.nome = nome;
+    }
+
+    public string getNome()
+    {
+      return this.nome;
+    }
+
+    public void setCpf(string cpf)
+    {
+      this.cpf = cpf;
+    }
+
+    public string getCpf()
+    {
+      return this.cpf;
+    }
+
+    public void setProfissao(string profissao)
+    {
+      this.profissao = profissao;
+    }
+
+    public string getProfissao()
+    {
+      return this.profissao;
+    }
+  }
+}
