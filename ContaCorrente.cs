7f24fb42af6bf1850bd8488a2dba218aafcc4a71
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-12-26 14:44:27 -0300

    Commit 4

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -8,9 +8,79 @@ namespace bytebank
 {
   public class ContaCorrente
   {
-    public int numero_agencia;
-    public string conta;
-    public string titular;
-    public double saldo;
+    private int numero_agencia = 0;
+    private string conta = "";
+    private string titular = "";
+    private double saldo = 100;
+
+    public void Depositar(double valor)
+    {
+      this.saldo += valor;
+    }
+
+    public bool Sacar(double valor)
+    {
+      if (valor <= this.saldo)
+      {
+        this.saldo -= valor;
+        return true;
+      }
+      else { 
+        return false;
+      }
+    }
+
+    public void setSaldo(double saldo)
+    {
+      this.saldo = saldo;
+    }
+
+    public double getSaldo()
+    {
+      return this.saldo;
+    }
+
+    public bool Transferir(double valor, ContaCorrente destino)
+    {
+      if (this.saldo < valor)
+      {
+        return false;
+      }
+      else { 
+        this.saldo -= valor;
+        destino.saldo += valor;
+        return true;
+      }
+    }
+    
+    public void setNumeroAgencia(int numero_agencia)
+    {
+      this.numero_agencia = numero_agencia;
+    }
+
+    public void setConta(string conta)
+    {
+      this.conta = conta;
+    }
+
+    public void setTitular(string titular)
+    {
+      this.titular = titular;
+    }
+
+    public int getNumeroAgencia()
+    {
+      return this.numero_agencia;
+    }
+
+    public string getConta()
+    {
+      return this.conta;
+    }
+
+    public string getTitular()
+    {
+      return this.titular;
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,44 @@ class Program
 {
   static void Main(string[] args)
   {
-    ContaCorrente contaCorrente = new ContaCorrente();
+    // ContaCorrente contaCorrente = new ContaCorrente();
+    // contaCorrente.Depositar(100);
+    // if (contaCorrente.Sacar(100))
+    // {
+    //   Console.WriteLine(contaCorrente.getSaldo());
+    // }
+    // else {
+    //   Console.WriteLine("Valor invalido");
+    // }
+
+    ContaCorrente contaDaMaria = new ContaCorrente();
+    ContaCorrente contaDoAndre  = new ContaCorrente();
+
+    contaDoAndre.setTitular("André Silva");
+    contaDoAndre.setNumeroAgencia(15);
+    contaDoAndre.setConta("1010-X");
+    contaDoAndre.setSaldo(100);
+    Console.WriteLine("Saldo da conta do André = " + contaDoAndre.getSaldo());
+
+    contaDaMaria.setTitular("Maria Souza");
+    contaDaMaria.setNumeroAgencia(17);
+    contaDaMaria.setConta("1010-5");
+    contaDaMaria.setSaldo(350);
+    Console.WriteLine("Saldo da conta d Maria = " + contaDaMaria.getSaldo());
+
+    contaDoAndre.Transferir(50, contaDaMaria);
+
+    Console.WriteLine("Saldo da conta do André = " + contaDoAndre.getSaldo());
+    Console.WriteLine("Saldo da conta d Maria = " + contaDaMaria.getSaldo());
+
+    ContaCorrente contaDoPedro = new ContaCorrente();
+    contaDoPedro.setTitular("Pedro");
+    contaDoPedro.setNumeroAgencia(18);
+    contaDoPedro.setConta("1010-9");
+    contaDoPedro.setSaldo(0);
+    Console.WriteLine(contaDoPedro.getTitular());
+    Console.WriteLine(contaDoPedro.getNumeroAgencia());
+    Console.WriteLine(contaDoPedro.getConta());
+    Console.WriteLine(contaDoPedro.getSaldo());
   }
 }
